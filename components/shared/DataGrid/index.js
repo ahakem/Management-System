67c4e38eb96a4cdff21cd850b9b4fc8ec6437caf
Hakim <me@hakim.ninja>
2021-01-23 00:07:10 +0200

    clean up

--- components/shared/DataGrid/index.js
+++ components/shared/DataGrid/index.js
@@ -8,18 +8,7 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import Actions from 'components/shared/DataGrid/Actions'
 const DataGrid = () => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [sortType, setSortType] = React.useState('date');
-  const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = (type) => {
-    setAnchorEl(null);
-    setSortType(type)
-  };
 
   return (
     <><Actions/></>
@@ -30,29 +19,7 @@ export default DataGrid;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
+   
   },
-  pagenationNo: {
-    marginRight: theme.spacing(2),
-    color: theme.palette.secondary.main,
-    fontWeight: "normal",
-    fontSize: 14,
-  },
-  pagenationBtns: {
-    minHeight: theme.spacing(6),
-    minWidth: theme.spacing(6),
-    background: "#fff",
-  },
-  sortBtn:{
-    marginLeft:theme.spacing(2),
-    minWidth:theme.spacing(17),
-    textTransform:"capitalize",
-    display:"flex",
-    justifyContent:"space-between"
-  },
-  menu:{
-    marginTop:theme.spacing(7),
-  }
+  
 }));
