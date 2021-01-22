0fb21eca68bf0197d72ebdcada52b03af316f750
Hakim <me@hakim.ninja>
2021-01-22 15:57:23 +0200

    TextField custum design

---
+++ components/shared/TextField.js
@@ -0,0 +1,27 @@
+import { withStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+
+const TextField = withStyles((theme) => ({
+  root: {
+    border: "1px solid red",
+    backgroundColor: theme.palette.common.white,
+    borderRadius: 4,
+    border: "1px solid #ced4da",
+    "label + &": {
+      // marginTop: theme.spacing(3),
+    },
+  },
+
+  input: {
+    position: "relative",
+    fontSize: 16,
+    width: "100%",
+    padding: "10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+
+    "&:focus": {
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}))(InputBase);
+export default TextField;
