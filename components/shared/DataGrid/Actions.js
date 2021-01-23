0418d2a3a8deee692ad6208ac5c0240df3fa415c
Hakim <me@hakim.ninja>
2021-01-23 15:46:38 +0200

    fix initial sort

--- components/shared/DataGrid/Actions.js
+++ components/shared/DataGrid/Actions.js
@@ -1,15 +1,29 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, ButtonGroup, Button, Typography, Menu, MenuItem, Fade } from "@material-ui/core";
+import {
+  Box,
+  ButtonGroup,
+  Button,
+  Typography,
+  Menu,
+  MenuItem,
+  Fade,
+} from "@material-ui/core";
 import ArrowBackIosOutlinedIcon from "@material-ui/icons/ArrowBackIosOutlined";
 import ArrowForwardIosOutlinedIcon from "@material-ui/icons/ArrowForwardIosOutlined";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
-const Actions = ({handleRequestSort, count, page, onChangePage, rowsPerPage}) => {
+const Actions = ({
+  handleRequestSort,
+  count,
+  page,
+  onChangePage,
+  rowsPerPage,
+}) => {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
-  const [sortType, setSortType] = useState('date');
+  const [sortType, setSortType] = useState("date");
   const open = Boolean(anchorEl);
 
   const handleClick = (event) => {
@@ -18,42 +32,76 @@ const Actions = ({handleRequestSort, count, page, onChangePage, rowsPerPage}) =>
 
   const handleClose = (type) => {
     setAnchorEl(null);
-    type != sortType && setSortType(type)
+    type != sortType && setSortType(type);
   };
-
+  const Lastrow = (page + 1) * rowsPerPage;
   return (
     <Box my={4} className={classes.root}>
-      <Typography className={classes.pagenationNo}>{((page+1) * rowsPerPage)- rowsPerPage + 1}-{(page+1) * rowsPerPage} of {count}</Typography>
+      <Typography className={classes.pagenationNo}>
+        {(page + 1) * rowsPerPage - rowsPerPage + 1}-
+        {Lastrow > count ? count : Lastrow} of {count}
+      </Typography>
       <ButtonGroup
         className={classes.pagenationBtns}
         color="secondary"
         aria-label="outlined secondary button group"
       >
-        <Button disabled={page == 0} onClick={()=>{onChangePage(page-1)}}>
+        <Button
+          disabled={page == 0}
+          onClick={() => {
+            onChangePage(page - 1);
+          }}
+        >
           <ArrowBackIosOutlinedIcon />
         </Button>
-        <Button disabled={Math.floor(count/rowsPerPage) == page} onClick={()=>{onChangePage(page+1)}}>
+        <Button
+          disabled={Math.ceil(count / rowsPerPage) == page + 1}
+          onClick={() => {
+            onChangePage(page + 1);
+          }}
+        >
           <ArrowForwardIosOutlinedIcon />
         </Button>
       </ButtonGroup>
       <div>
-      <Button className={classes.sortBtn} variant="outlined" aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
-        <span>Sort: <span style={{color:"#000"}}>{sortType}</span></span>
-        {!anchorEl ? <ExpandMoreIcon /> : <ExpandLessIcon/>}
-      </Button>
-      <Menu
-        id="fade-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Fade}
-        className={classes.menu}
-      >
-        <MenuItem className={classes.menuItem} onClick={()=>{handleRequestSort("date"),handleClose("date")}}>Date</MenuItem>
-        <MenuItem onClick={()=>{handleRequestSort("status"),handleClose("status")}}>status</MenuItem>
-      </Menu>
-    </div>
+        <Button
+          className={classes.sortBtn}
+          variant="outlined"
+          aria-controls="fade-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <span>
+            Sort: <span style={{ color: "#000" }}>{sortType}</span>
+          </span>
+          {!anchorEl ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+        </Button>
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+          className={classes.menu}
+        >
+          <MenuItem
+            className={classes.menuItem}
+            onClick={() => {
+              handleRequestSort("date"), handleClose("date");
+            }}
+          >
+            Date
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleRequestSort("status"), handleClose("status");
+            }}
+          >
+            status
+          </MenuItem>
+        </Menu>
+      </div>
     </Box>
   );
 };
@@ -77,18 +125,17 @@ const useStyles = makeStyles((theme) => ({
     minWidth: theme.spacing(6),
     background: "#fff",
   },
-  sortBtn:{
-    marginLeft:theme.spacing(2),
-    minWidth:theme.spacing(17),
-    textTransform:"capitalize",
-    display:"flex",
-    justifyContent:"space-between"
+  sortBtn: {
+    marginLeft: theme.spacing(2),
+    minWidth: theme.spacing(17),
+    textTransform: "capitalize",
+    display: "flex",
+    justifyContent: "space-between",
   },
-  menu:{
-    marginTop:theme.spacing(7),
-    
+  menu: {
+    marginTop: theme.spacing(7),
   },
-  menuItem:{
-    minWidth:theme.spacing(17),
-  }
+  menuItem: {
+    minWidth: theme.spacing(17),
+  },
 }));
--- components/shared/DataGrid/index.js
+++ components/shared/DataGrid/index.js
@@ -7,51 +7,11 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TableSortLabel,
   Paper,
 } from "@material-ui/core";
 import moment from "moment";
 import Actions from "components/shared/DataGrid/Actions";
 import { axios, urls } from "config/axios";
-
-function createData(name, date, fat, carbs, protein) {
-  return { name, date, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Active1", new Date("2021-2-1"), 25.0, 51, 4.9),
-  createData("InShop2", new Date("2021-1-1"), 16.0, 24, 6.0),
-  createData("Active3", new Date("2021-1-1"), 3.7, 67, 4.3),
-  createData("OutOfService4", new Date("2021-1-5"), 6.0, 24, 4.0),
-  createData("Active5", new Date("2021-1-1"), 16.0, 49, 3.9),
-  createData("InShop6", new Date("2021-1-5"), 3.2, 87, 6.5),
-  createData("InShop7", new Date("2021-2-1"), 9.0, 37, 4.3),
-  createData("Active8", new Date("2021-1-7"), 0.0, 94, 0.0),
-  createData("InShop9", new Date("2021-1-1"), 26.0, 65, 7.0),
-  createData("Active10", new Date("2021-1-7"), 0.2, 98, 0.0),
-  createData("OutOfService11", new Date("2021-1-12"), 0, 81, 2.0),
-  createData("OutOfService12", new Date("2021-1-12"), 19.0, 9, 37.0),
-  createData("Active13", new Date("2021-1-1"), 18.0, 63, 4.0),
-];
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    position: "relative",
-  },
-  paper: {
-    overflowX: "auto",
-    marginBottom: theme.spacing(2),
-    position: "absolute",
-    top: 64,
-    minHeight: 200,
-    left: 0,
-    right: 0,
-  },
-  table: {
-    minWidth: 750,
-  },
-}));
 
 export default function DataGrid() {
   const classes = useStyles();
@@ -60,25 +20,22 @@ export default function DataGrid() {
   const [data, setData] = useState([]);
   const [vehiclesNames, SetVehiclesNames] = useState({});
 
-  const rowsPerPage = 5;
+  const rowsPerPage = 7;
   const handleChangePage = (newPage) => {
     setPage(newPage);
   };
 
-  const SortData = (property) => {
-    
+  const SortData = (property, _data = data) => {
     const compare = (a, b) => (a[property] < b[property] ? -1 : 1);
-
-    setData([...data.sort(compare)]);
+    setData([..._data.sort(compare)]);
   };
 
   useEffect(() => {
     axios
       .get(urls.vehicles)
       .then((res) => {
-        setData(res.data.data.vehicles_info);
         SetVehiclesNames(res.data.data.vehicles_names);
-        SortData("date")
+        SortData("date", res.data.data.vehicles_info);
       })
       .catch((error) => {
         console.log(error);
@@ -87,7 +44,9 @@ export default function DataGrid() {
         //
       });
   }, []);
- 
+  useEffect(() => {
+    console.log("data", data);
+  });
   let currentDate = null;
   return (
     <div className={classes.root}>
@@ -127,17 +86,17 @@ export default function DataGrid() {
                   }
                   return (
                     <React.Fragment key={row.id}>
-                      {head &&
-                      <TableRow>
-                        <TableCell colSpan={6} scope="row">
-                          <b>{moment(row.date).format("ddd, MMM Do YYYY")}</b>
-                        </TableCell>
+                      {head && (
+                        <TableRow>
+                          <TableCell colSpan={6} scope="row">
+                            <b>{moment(row.date).format("ddd, MMM Do YYYY")}</b>
+                          </TableCell>
                         </TableRow>
-                        }
-                      <TableRow hover >
+                      )}
+                      <TableRow hover>
                         <TableCell scope="row">
-                          {vehiclesNames[row.vehicle_id]} - 
-                          <b>{row.status}</b> - {row.id}
+                          {vehiclesNames[row.vehicle_id]} -<b>{row.status}</b> -{" "}
+                          {row.id}
                         </TableCell>
                         <TableCell>{row.time}</TableCell>
                         {/* <TableCell align="right">
@@ -158,3 +117,21 @@ export default function DataGrid() {
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "relative",
+  },
+  paper: {
+    overflowX: "auto",
+    marginBottom: theme.spacing(2),
+    position: "absolute",
+    top: 64,
+    minHeight: 200,
+    left: 0,
+    right: 0,
+  },
+  table: {
+    minWidth: 750,
+  },
+}));
